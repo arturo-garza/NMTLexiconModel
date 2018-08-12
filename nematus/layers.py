b69fa454bc656ec80fc45faf54e8543efa0a9caa
Erick Garza <s1723861@ed.ac.uk>
2018-08-12 13:58:25 +0100

    Softmax for alignments

--- nematus/layers.py
+++ nematus/layers.py
@@ -522,7 +522,7 @@ class AttentionStep(object):
         
         #egarza - calculate c_embeds
         scores_out = tf.nn.softmax(scores)
-        scores_out =  tf.multiply(tf.expand_dims(scores, axis=2), src_embs)
+        scores_out =  tf.multiply(tf.expand_dims(scores_out, axis=2), src_embs)
         scores_out = tf.reduce_sum(scores_out, axis=0, keep_dims=False)
         #scores_out = tf.nn.softmax(scores_out)
 
